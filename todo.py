8f7111bd868c969345a095cfebe047f337e9b352
akush <piyushpandey2062@gmail.com>
2026-01-21 22:31:37 +0545

    changed delete case handler to completed and stored todos in seprate file 'todos.txt'

--- todo.py
+++ todo.py
@@ -1,44 +1,63 @@
-Todo_list= [] # Declated a empty list
-
 while True :
 
-    user_prompt= input("enter prompts: add. show, edit, delete, or exit: ")
+    user_prompt= input("enter prompts: add, show, edit, complete, or exit: ")
+    user_prompt = user_prompt.strip()
 
     match user_prompt :
         case "add" :
-            todos= input("write your things to do: ")
-            Todo_list.append(todos.title())
+            
+            todos = input("Enter your todos: ") + "\n"  
+            
+            with open("todos.txt", "r") as file :
+                todo = file.readlines()                   
+            
+            todo.append(todos.title())                     
+           
+            
+            with open("todos.txt", "w") as file :
+                file.writelines(todo)
+
+          
         
         case "show" :
-            print("-" *50)
-            for i in range(0, len(Todo_list)):
-                print(f"{i+1}. {Todo_list[i]}")
-            print("-" *50)
-
-        case "delete" :
-            ndx= int(input("enter index of todo items "))
-
-            if 1 <= ndx <= len(Todo_list): 
-                Todo_list.pop(ndx-1)
-                print("deleted")
-
-            else :
-                print(f"invalid index index cant be greater than length of list")
+            with open('todos.txt', 'r') as file :      
+                todos_list= file.readlines()         
+            
+            
+            todos= [w.strip("\n") for w in todos_list]      
+            
+            for i, list_element in enumerate(todos, start=1):      
+                print(i, list_element)   
 
         case "edit" :
-            ndx= int(input("index of todo items to edit: "))
+            ndx= int(input("Number of todo items to edit: "))
+            ndx= ndx-1
 
-            if 1 <= ndx <= len(Todo_list) :
-                newtodo= input("change your todo: ")
-                Todo_list[ndx-1]=newtodo.title()
+            with open("todos.txt", "r") as file :
+                todos= file.readlines()
+            
+            new_todos = input("Enter new todo: ")
+            todos[ndx]= new_todos.title() +'\n'
 
-            else:
-                print("Invalid index")
+            with open('todos.txt', 'w') as file :
+                file.writelines(todos)
+
+        case "complete" :
+            
+            ndx= int(input("Enter number of todo items you completed: "))
+            ndx= ndx-1
+
+            with open('todos.txt','r') as file :
+                todos= file.readlines()
+            
+            todos.pop(ndx)
+
+            with open('todos.txt','w') as file :
+                file.writelines(todos)
+            
 
         case "exit" :
             break
 
         case _:
-            print("Invalid prompt \n valid ones are add, show, edit, delete, or exit")
-
-        
+            print("Invalid prompt \n valid ones are add, show, edit, delete, or exit")    
